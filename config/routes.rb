d6e417d7c651af23cc4ad1f37868b9bc9877ce41
Lucas Colwell <colwellc37@gmail.com>
2023-03-01 13:30:30 -0700

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :subscriptions, only: %i[create update]
+      resources :customers, only: %i[index show] do
+        get '/subscriptions', to: 'customer_subscriptions#index'
+      end
     end
   end
 end
