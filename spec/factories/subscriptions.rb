88c756856990250f4b2903648b921015ce89f81f
Lucas Colwell <colwellc37@gmail.com>
2023-02-28 17:41:41 -0700

    Test

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :subscription do
-    title { "MyString" }
-    price { 1 }
-    status { 1 }
-    frequency { 1 }
-    tea { nil }
-    customer { nil }
+    title { "#{customer.first_name} #{customer.last_name}'s #{tea.title}" }
+    price { Faker::Number.within(range: 3..20) }
+    status { [0, 1].sample }
+    frequency { [0, 1, 2, 3].sample }
+    tea { create(:tea) }
+    customer { create(:customer) }
   end
 end
